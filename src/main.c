e773c9ba5ce9aacf5415e9998d083aac61591b62
Tsuchu <abilbro005@gmail.com>
2024-10-14 17:31:07 -0400

    weird shit

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(void) {
-    printf("Hello World!\n");
-    printf("Hello World 2!\n");
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);
+    int rv = bind(fd, (const struct sockaddr *) &addr, sizeof(addr));
+    if (rv) die("bind()");
+
+    rv = listen(fd, SOMAXCONN);
+    if (rv) die("listen()");
 }
